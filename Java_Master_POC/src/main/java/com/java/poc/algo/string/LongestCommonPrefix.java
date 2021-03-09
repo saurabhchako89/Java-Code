fbb852381935c80daf219a4a84c9c7f9127f8945
saurabhchako89 <saurabhchako89@gmail.com>
2021-03-08 23:08:06 -0600

    code

--- Java_Master_POC/src/main/java/com/java/poc/algo/string/LongestCommonPrefix.java
+++ Java_Master_POC/src/main/java/com/java/poc/algo/string/LongestCommonPrefix.java
@@ -1,4 +1,26 @@
 package com.java.poc.algo.string;
 
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"faxyz", "fanbxyz","fabxyz", "fabcxyz"};
+        checkLongestCommonPrefix(strs);
+    }
+
+    private static void checkLongestCommonPrefix(String[] strs) {
+        System.out.println("unsorted array is : "+ Arrays.toString(strs));
+        Arrays.sort(strs);
+        System.out.println("sorted array is : "+ Arrays.toString(strs));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < strs.length-1;i++){
+            if(strs[0].charAt(i) == strs[strs.length - 1].charAt(i)){
+                sb.append(strs[0].charAt(i));
+            }else{
+                break;
+            }
+        }
+
+        System.out.println("longest common prefix is : "+sb.toString());
+    }
 }

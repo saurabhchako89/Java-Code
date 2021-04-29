dc9b4cd459bb33ca15bc8157bdd894b670a0ea18
saurabhchako89 <saurabhchako89@gmail.com>
2021-04-29 11:02:44 -0500

    code

---
+++ Java_Master_POC/src/main/java/com/java/poc/algo/greedy/MaximunWaterBottlesToDrink.java
@@ -0,0 +1,21 @@
+package com.java.poc.algo.greedy;
+
+public class MaximunWaterBottlesToDrink {
+    public static void main(String[] args) {
+        int numBottles = 3;
+        int numExchange = 4;
+        System.out.println("Number of water bottles which you can drink : "+numWaterBottles(numBottles,numExchange));
+    }
+
+    public static int numWaterBottles(int numBottles, int numExchange) {
+        int drink = numBottles;
+        int numFull;
+
+        while(numBottles >= numExchange) {
+            numFull = numBottles/numExchange;
+            drink = drink+ numFull;
+            numBottles = numFull+(numBottles%numExchange);
+        }
+        return drink;
+    }
+}

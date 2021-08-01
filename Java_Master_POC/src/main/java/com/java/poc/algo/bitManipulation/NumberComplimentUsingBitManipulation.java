24817a46b43f97b201e2e4beb75baf33c0532801
saurabhchako89 <saurabhchako89@gmail.com>
2021-08-01 13:07:34 -0500

    weekly commit

--- Java_Master_POC/src/main/java/com/java/poc/algo/bitManipulation/NumberComplimentUsingBitManipulation.java
+++ Java_Master_POC/src/main/java/com/java/poc/algo/bitManipulation/NumberComplimentUsingBitManipulation.java
@@ -3,6 +3,8 @@ package com.java.poc.algo.bitManipulation;
 public class NumberComplimentUsingBitManipulation {
 
     public static void main(String[] args) {
-
+        int num = 2;
+        System.out.println("number is :"+num);
+        System.out.println("compliment is :"+ ~num);
     }
 }
---
+++ Java_Master_POC/src/main/java/com/java/poc/algo/bitManipulation/subtract2NumbersWithoutSubtractionOperator.java
@@ -0,0 +1,25 @@
+package com.java.poc.algo.bitManipulation;
+
+public class subtract2NumbersWithoutSubtractionOperator {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 7;
+
+        System.out.println("Subtracting "+a+" from "+b+" is : "+addNums(a,b));
+    }
+
+    private static int addNums(int a, int b) {
+        if(a==0){
+            return b;
+        }
+        if(b==0){
+            return a;
+        }
+        while(b!=0) {
+            int carry = ~a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}

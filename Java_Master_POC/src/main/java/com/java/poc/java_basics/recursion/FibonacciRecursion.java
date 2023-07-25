a0a30895c6b72900346fe2ca1c0b34a4327b537a
saurabhchako89 <saurabhchako89@gmail.com>
2023-07-25 11:40:00 -0500

    code

--- Java_Master_POC/src/main/java/com/java/poc/java_basics/recursion/FibonacciRecursion.java
+++ Java_Master_POC/src/main/java/com/java/poc/java_basics/recursion/FibonacciRecursion.java
@@ -2,12 +2,19 @@ package com.java.poc.java_basics.recursion;
 
 public class FibonacciRecursion {
     public static void main(String[] args) {
-        int num = 5;
+        int num = 10;
+        System.out.println(num+"th fibonacci number is : "+getNthFibonacci(num));
 
     }
 
-    public static void printNthFibonacci(int num){
+    public static int getNthFibonacci(int num){
+        if(num <=1){
+            return num;
+        }
+        int n_1 = getNthFibonacci(num-1);
+        int n_2 = getNthFibonacci(num-2);
 
+        return n_1+n_2;
     }
 
 }
